752003482a76454b1807926b14735eefd02bb7e4
ajay910 <33267658+ajay910@users.noreply.github.com>
2018-04-18 23:40:54 -0400

    Add files via upload

---
+++ BackgroundTasksFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace App5.UWP.BackgroundTaskHandler
+{
+    public class BackgroundTasksFactory
+    {
+        public static BackgroundTaskRegistration RegisterBackgroundTask(string name, IBackgroundTrigger trigger, IBackgroundCondition condition)
+        {
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+
+                if (cur.Value.Name == name)
+                    // The task is already registered.
+                    return (BackgroundTaskRegistration)(cur.Value);
+            }
+
+            //Register new background task:
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = name;
+            //builder.TaskEntryPoint = taskEntryPoint;
+            //SetTrigger()
+            
+            builder.SetTrigger(trigger);
+            builder.AddCondition(new SystemCondition(SystemConditionType.UserPresent));
+
+            if (condition != null)
+            {
+                builder.AddCondition(condition);
+            }
+
+            BackgroundTaskRegistration task = builder.Register();
+
+            return task;
+
+
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App5.UWP.BackgroundTaskHandler;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace App5.UWP
+{
+   
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+          
+            LoadApplication(new App5.App());
+            BackgroundTasksFactory.RegisterBackgroundTask("MyBackgroundTask", new SystemTrigger(SystemTriggerType.InternetAvailable, true), null);
+        }
+    }
+}
