c6e2a08d56436260f97625db4f515eb85f087d1d
ajay910 <33267658+ajay910@users.noreply.github.com>
2018-05-02 22:53:19 -0400

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,63 +1,32 @@
-﻿using Newtonsoft.Json;
+﻿using App5.UWP.BackgroundTaskHandler;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-namespace App5
+namespace App5.UWP
 {
-    public class JsonConv
-    {
-        public List<ApplicationLog> ApplicationLogs { get; set; } 
-    }
-    public class ApplicationLog
-    {
-        public string DeviceId { get; set; }
-        public string ComputerName { get; set; }
-        public DateTime TimeStamp { get; set; }
-    }
-    public partial class MainPage : ContentPage
+   
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            InitializeComponent();
-            DisplayAlert("as", "asd", "OK");
-            IServiceClass obj = new ServiceClass();
-            obj.CallDatabase();
+            this.InitializeComponent();
 
-            //var a = GetData();
-            //var data = a.stringList;
-            //int i;
-            int.TryParse("2", out int i);
-            var b = i;
-
-            
-            var strin = "{ApplicationLogs:[{DeviceId:\"1\",LogTimeStamp:\"01-01-2015\",ComputerName:\"\",TimeStamp:\"01-01-0001\",Time:\"01-01-0001\"},{DeviceId:\"1\",LogTimeStamp:\"01-01-2015\",ComputerName:\"not null\",TimeStamp:\"01-01-0001\",Time:\"01-01-0001\"}]}";
-            var convertedData = JsonConvert.DeserializeObject<JsonConv>(strin);
-            foreach(var item in convertedData.ApplicationLogs)
-            {
-                if (string.IsNullOrEmpty(item.ComputerName)){
-                    item.ComputerName = "Server Name";
-                }
-
-                if(item.TimeStamp != null && item.TimeStamp.Year < 1732)
-                {
-                    item.TimeStamp = DateTime.Now;
-                }
-            }
-            //SetLiencence(b.number)
+          
+            LoadApplication(new App5.App());
+            BackgroundTasksFactory.RegisterBackgroundTask("MyBackgroundTask", new SystemTrigger(SystemTriggerType.InternetAvailable, true), null);
         }
-
-        public void SetLiencence(string number )
-        {
-           // texbox.text = number;
-        }
-
-        //public List<int> intList GetData()
-        //{
-        //    return new List<int> { 10 };
-        //}
     }
 }
