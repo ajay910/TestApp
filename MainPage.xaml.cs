fb1f595301cff7db0d9fd2dbd2b3928479b0a0a8
ajay910 <33267658+ajay910@users.noreply.github.com>
2018-05-02 22:52:15 -0400

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,32 +1,63 @@
-﻿using App5.UWP.BackgroundTaskHandler;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Background;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
-namespace App5.UWP
+namespace App5
 {
-   
-    public sealed partial class MainPage
+    public class JsonConv
+    {
+        public List<ApplicationLog> ApplicationLogs { get; set; } 
+    }
+    public class ApplicationLog
+    {
+        public string DeviceId { get; set; }
+        public string ComputerName { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+    public partial class MainPage : ContentPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            DisplayAlert("as", "asd", "OK");
+            IServiceClass obj = new ServiceClass();
+            obj.CallDatabase();
 
-          
-            LoadApplication(new App5.App());
-            BackgroundTasksFactory.RegisterBackgroundTask("MyBackgroundTask", new SystemTrigger(SystemTriggerType.InternetAvailable, true), null);
+            //var a = GetData();
+            //var data = a.stringList;
+            //int i;
+            int.TryParse("2", out int i);
+            var b = i;
+
+            
+            var strin = "{ApplicationLogs:[{DeviceId:\"1\",LogTimeStamp:\"01-01-2015\",ComputerName:\"\",TimeStamp:\"01-01-0001\",Time:\"01-01-0001\"},{DeviceId:\"1\",LogTimeStamp:\"01-01-2015\",ComputerName:\"not null\",TimeStamp:\"01-01-0001\",Time:\"01-01-0001\"}]}";
+            var convertedData = JsonConvert.DeserializeObject<JsonConv>(strin);
+            foreach(var item in convertedData.ApplicationLogs)
+            {
+                if (string.IsNullOrEmpty(item.ComputerName)){
+                    item.ComputerName = "Server Name";
+                }
+
+                if(item.TimeStamp != null && item.TimeStamp.Year < 1732)
+                {
+                    item.TimeStamp = DateTime.Now;
+                }
+            }
+            //SetLiencence(b.number)
         }
+
+        public void SetLiencence(string number )
+        {
+           // texbox.text = number;
+        }
+
+        //public List<int> intList GetData()
+        //{
+        //    return new List<int> { 10 };
+        //}
     }
 }
